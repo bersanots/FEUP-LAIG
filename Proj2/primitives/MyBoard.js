8834089d31846d3d91ea7cef30d0fb820d1df786
Bruno Vale Fernandes <up200707284@g.uporto.pt>
2019-12-13 10:31:03 +0000

    Boardie board

--- Proj2/primitives/MyBoard.js
+++ Proj2/primitives/MyBoard.js
@@ -11,7 +11,6 @@ class MyBoard extends CGFobject {
 
     createBoard() {
         // for (var i = 0; i < 2; i++) {
-            // console.log("Alabama");
             this.board = new MyCell(this.scene, this.radius);
             
         // }
@@ -20,15 +19,21 @@ class MyBoard extends CGFobject {
 
     drawBoard() {
         this.scene.rotate(-Math.PI/2, 0, 0, 1);
-        for (let i = 0; i < 5; ++i) { 
-            let j = 0;
-            for (j = 0; j < 5; ++j) {
-                this.drawCells();
-                this.scene.translate(0, this.radius*1.74, 0);
-            }
 
-            this.scene.translate(1, -j, 0);
-        }
+        let i = 5;
+        let a = false;
+        // do{
+        //     let j = 0;
+        //     for (j; j < i-1; ++j) {
+        //         this.drawCells();
+        //         this.scene.translate(0, this.radius*1.74, 0);
+        //     }
+        //     this.scene.translate(1, -j*0.9, 0);
+        //     (i==9)? a = true: a = false;
+        //     a ? ++i : --i;
+        //     console.log(i);
+        // }while(i != 5);
+
     }
 
     drawCells() {
